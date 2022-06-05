b0447c1d95da240976942548f2aae742b7d3bae5
Tsa2341 <alanshema2002@gmail.com>
2022-06-05 12:15:30 +0200

    added firebase firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideRemoteConfig(() => getRemoteConfig())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'myportifolio-backend',
+    appId: '1:470599192196:web:94d808aa92e6f99d883cd1',
+    storageBucket: 'myportifolio-backend.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBvI-9V2HEY7UfX6GiNtmHDnMSZsu29BAk',
+    authDomain: 'myportifolio-backend.firebaseapp.com',
+    messagingSenderId: '470599192196',
+    measurementId: 'G-84HRCVENN1',
+  },
   production: false
 };
 
